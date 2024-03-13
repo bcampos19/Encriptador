4ca1ec04093180dee3191b6f62139701a5314c99
Benjamin Campos <62151728+bcampos19@users.noreply.github.com>
2024-03-12 19:10:01 -0500

    Add files via upload
    Actualización por error en james

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ let campo_mensaje  = document.getElementById('campoMensaje');
 //let campo_mensaje  = document.querySelector("#campoMensaje");
 
 
-const matriz_code = [
-    ["e","enter"], //indice 0 
-    ["i","imes"],  //indice 1
-    ["a","ai"],    //indice 2
-    ["o","ober"],  //indice 3
-    ["u","ufat"],  //indice 4
+const matriz_code = [    
+    ["e","enter"], //indice 1
+    ["i","imes"],  //indice 2 
+    ["a","ai"],    //indice 3   
+    ["o","ober"],  //indice 4
+    ["u","ufat"],  //indice 5
 ];
 
 
 function btnEncriptar(){
-    campo_mensaje.value = encriptar(campo_texto.value);
+    campo_mensaje.value = encriptar(campo_texto.value);    
 }
 
 function btnDesEncriptar(){
@@ -26,23 +26,25 @@ function btnDesEncriptar(){
 function encriptar(fraseEncriptada) {
     for(let i=0; i < matriz_code.length; i++) {
         if(fraseEncriptada.includes(matriz_code[i][0])){
-            fraseEncriptada = fraseEncriptada.replaceAll(matriz_code[i][0],matriz_code[i][1]);
+            fraseEncriptada = fraseEncriptada.replaceAll(matriz_code[i][0],'-'+matriz_code[i][1]);
         }
     }
-    return fraseEncriptada;
+    return fraseEncriptada.replaceAll('-', '');
 }
 
 
-function desencriptar(fraseDesencriptada) {
-    for(let i=0; i < matriz_code.length; i++) {
-        if(fraseDesencriptada.includes(matriz_code[i][1])){
-            fraseDesencriptada = fraseDesencriptada.replaceAll(matriz_code[i][1],matriz_code[i][0]);
+function desencriptar(fraseDesencriptada) {    
+    for(let i=0; i < matriz_code.length; i++) {        
+        if(fraseDesencriptada.includes(matriz_code[i][1])){            
+            fraseDesencriptada = fraseDesencriptada.replaceAll(matriz_code[i][1],'-'+matriz_code[i][0]);            
         }
     }
-    return fraseDesencriptada;
+    return fraseDesencriptada.replaceAll('-', '');
 }
 
 function btnCopiar() {
-    navigator.clipboard.writeText(campo_mensaje.value);    
+    navigator.clipboard.writeText(campo_mensaje.value);
+    campo_mensaje.value = '';
+
     //alert("Texto copiado: " + campo_mensaje.value);
 }
